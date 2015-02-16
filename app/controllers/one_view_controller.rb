7ce0f5199045fbbca739b26d75751af7ce140b63
Chris Lerum <chris.lerum@gmail.com>
2015-02-16 17:50:14 -0500

    x

--- app/controllers/one_view_controller.rb
+++ app/controllers/one_view_controller.rb
@@ -1,3 +1,15 @@
 class OneViewController < UIViewController
+  def loadView
+    @layout = OneLayout.new
+    self.view = @layout.view
 
+    @button_in_one = @layout.button_in_one
+  end
+
+  def viewDidLoad
+    @button_in_one.on(:touch) do
+      puts 'CCCCCCCCCCCC'
+    end
+
+  end
 end
---
+++ app/layouts/one_layout.rb
@@ -0,0 +1,14 @@
+class OneLayout < MK::Layout
+
+  view :button_in_one
+
+  def layout
+    @button_in_one = add UIButton, :button_in_one
+  end
+
+  def button_in_one_style
+    background_color "#0000ff".uicolor
+    frame [[0, 0], ['50%', 50]]
+    title 'PUSH'
+  end
+end
