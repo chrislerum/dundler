0b908b1d67429c1fa5a1bf5c57fbee3c0f4fcd68
Chris Lerum <chris.lerum@gmail.com>
2015-02-16 17:44:45 -0500

    x

--- app/layouts/boxes_layout.rb
+++ app/layouts/boxes_layout.rb
@@ -4,10 +4,16 @@ class BoxesLayout < MK::Layout
 
   def layout
     @button = add UIButton, :button
+    add OneViewController.new.view, :one
   end
 
   def button_style
     background_color "#00ff00".uicolor
     frame [[0, 200], ['50%', 100]]
+  end
+
+  def one_style
+    background_color "#ff0000".uicolor
+    frame [[0, 300], ['50%', 100]]
   end
 end
